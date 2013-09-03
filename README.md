9c0aa6c0ae388cc1d3c3c959f07c4820468dfee3
Tjaart van der Walt <github@tjaart.co.za>
2013-09-03 14:36:24 -0500

    Updated README + changed license to MIT (it was never published using GPL so it should be ok)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-quasi-interpolate
+Quasi Interpolate
 =================
+This is an implementation of the Quasi Interpolate algorithm as defined in the text book [MATHEMATICS OF APPROXIMATION](http://www.amazon.com/Mathematics-Approximation-Textbooks-Science-Engineering/dp/949121649X/ref=sr_1_1?ie=UTF8&qid=1378235841&sr=8-1&keywords=MATHEMATICS+OF+APPROXIMATION+by+Johan+de+Villiers) by Johan de Villiers.
+
+The algorithm is implemented in [Sage](http://www.sagemath.org/) and you will need to have Sage [installed](http://www.sagemath.org/doc/installation/) in order to run it on your system.
+
+I did not have a the time to nicely package this project. Feel free to contribute!
+A quick and dirty way to get the code working is to copy the algorithm (src/quasi_interpolate.py) into your code base. Look at the examples in the test directory for how to use it.
--- src/quasi_interpolator.py
+++ src/quasi_interpolator.py
@@ -1,20 +1,27 @@
 # This program calculates the quasi interpolate as defined in the book
 # MATHEMATICS OF APPROXIMATION by Johan de Villiers
+# You need Sage to run this program, See the README for more details
 
 # Copyright (C) 2013  Tjaart van der Walt
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 
 import sys
 import math
@@ -27,9 +34,9 @@ def quasi_interpolate(m, r, t):
   for j in range(-m, r + 1):
     innersum = 0
     for k in range(j, j + m + 1):
-      innersum += alpha(m, j, k)*f(k, t)
+      innersum += alpha(m, j, k, t)*f(k, t)
       
-    return_value += innersum * n(m, j, x)
+    return_value += innersum * b_spline(m, j, x, t)
   return return_value
 
 # From equation 10.4.32 on page 329
